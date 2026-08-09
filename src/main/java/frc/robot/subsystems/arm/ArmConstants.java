d759ab1aac9d02bde3a9505b84358ace6b323998
bhlcusd <242719234+bhlcusd@users.noreply.github.com>
2026-08-08 17:35:00 -0700

    remove arm

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.arm;
-
-public class ArmConstants {
-	public static final int LEFT_MOTOR_ID = 0;
-	public static final int RIGHT_MOTOR_ID = 1;
-	public static final int EXTEND_MOTOR_ID = 2;
-}
